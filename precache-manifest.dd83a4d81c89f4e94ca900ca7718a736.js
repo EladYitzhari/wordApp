c42931e4c5b76c79fb07475e42c43542ba86a96d
Elad Yitzhari <eladyzhari@gmail.com>
2019-10-31 15:39:53 +0200

    Updates

---
+++ precache-manifest.dd83a4d81c89f4e94ca900ca7718a736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4bc6549f4aa514fcba3ff6198ad870",
+    "url": "/wordApp/index.html"
+  },
+  {
+    "revision": "c08c01399da5057f2d32",
+    "url": "/wordApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7bc0bbc5cdd237827761",
+    "url": "/wordApp/static/js/2.5f6a3c86.chunk.js"
+  },
+  {
+    "revision": "c08c01399da5057f2d32",
+    "url": "/wordApp/static/js/main.8a6eadc7.chunk.js"
+  },
+  {
+    "revision": "44ddc83900dc3bc337fa",
+    "url": "/wordApp/static/js/runtime-main.9c9b52b5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/wordApp/static/media/logo.25bf045c.svg"
+  }
+]);
